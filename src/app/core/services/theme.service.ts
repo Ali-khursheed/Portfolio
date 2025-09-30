75d936d303ccdd1c42b9b8c81b464956322207d9
Ali khursheed <ali@applydreams.com>
2025-10-01 00:37:50 +0500

    Another problem solve

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,30 +1,29 @@
 import { Injectable, Signal, computed, signal } from '@angular/core';
-import { environment } from '@env/environment';
+// import { environment } from '@env/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
   public color = signal<string>(this.getInitialColor());
-  public openColor = signal<boolean>(false)
+  public openColor = signal<boolean>(false);
   public getColor: Signal<string> = computed(() => this.color());
-  setThemeColor(color: string){
+  setThemeColor(color: string) {
     this.color.set(color);
     this.saveColor(color);
   }
-  toggle(){
+  toggle() {
     this.openColor.set(!this.openColor());
   }
-  private saveColor(color: string){
+  private saveColor(color: string) {
     (localStorage as any).color = color;
   }
   private getInitialColor(): string {
     if (typeof window !== 'undefined') {
       const storedColor = localStorage.getItem('color');
-      if(storedColor){
+      if (storedColor) {
         return storedColor;
       }
     }
-    return environment.mainColor
-}
-
+    return 'nothing';
+  }
 }
